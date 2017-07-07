1f04606e79055905468d155e3435eef92706afd4
naokiur <atomic.transfer.comp@gmail.com>
2017-07-07 09:09:39 +0900

    Add tutolial [3.1.1. Numbers].

---
+++ number3.1.1/__init__.py
@@ -0,0 +1,46 @@
+# Example Addition
+calc1 = 2 + 2
+print(calc1)
+
+# Example Multiple
+calc2 = 50 - 5 * 6
+print(calc2)
+
+calc3 = (50 - 5 * 6) / 4
+print(calc3)
+
+calc4 = 8 / 5
+print(calc4)
+
+calc5 = 17 / 3
+print(calc5)
+
+calc6 = 17 // 3
+print(calc6)
+
+calc7 = 17 % 3
+print(calc7)
+
+calc8 = 5 * 3 + 2
+print(calc8)
+
+squared1 = 5 ** 2
+print(squared1)
+
+squared2 = 2 ** 7
+print(squared2)
+
+# Example using variables
+width = 20
+height = 5 * 9
+result = width * height
+print(result)
+
+# Example not defined
+# print(n) <- This is compile error or runtime error.
+
+# Example auto boxing
+calc9 = 3 * 3.75 / 1.5
+print(calc9)
+calc10 = 7.0 / 2
+print(calc10)
