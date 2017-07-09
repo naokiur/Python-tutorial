0037ed6cc41ff9c51456f6ae769e9cd6599eb850
naokiur <atomic.transfer.comp@gmail.com>
2017-07-09 17:45:19 +0900

    Add tutorial [3.2. First Steps Towards Programming.]

---
+++ firststep3.2/__init__.py
@@ -0,0 +1,15 @@
+a = 0
+b = 1
+
+while b < 10:
+    print(b)
+    a = b
+    b = a + b
+
+i = 256 * 256
+print('The value of i is', i)
+
+a, b = 0, 1
+while b < 1000:
+    print(b, end=',')
+    a, b = b, a + b
