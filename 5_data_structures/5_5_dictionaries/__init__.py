577a565becb9f7ada9290c243eada39941e404ab
naokiur <atomic.transfer.comp@gmail.com>
2017-07-22 15:18:46 +0900

    Add tutorial [5.5. Dictionaries]

---
+++ 5_data_structures/5_5_dictionaries/__init__.py
@@ -0,0 +1,20 @@
+tel = {'jack': 4098, 'sape': 4139}
+tel['guido'] = 4127
+print(tel)
+
+print(tel['jack'])
+
+del tel['sape']
+print(tel)
+tel['irv'] = 4127
+print(tel)
+
+print(list(tel.keys()))
+print(sorted(list(tel.keys())))
+
+print('guido' in tel)
+print('Jack' in tel)
+
+print(dict([('sape', 4139), ('guido', 4127), ('jack', 4098)]))
+print({x: x**2 for x in (2, 4, 6)})
+print(dict(sape=4139, guido=4127, jack=4098))
